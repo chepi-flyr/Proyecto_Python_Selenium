f0eb6b56a2d09136b3ee0321a3ca193776463894
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2025-10-12 14:32:59 -0600

    Update test1.py

--- src/Test/test1.py
+++ src/Test/test1.py
@@ -31,25 +31,6 @@ class GoogleTest(unittest.TestCase):
         self.driver.find_element("xpath","//input[@id='cb1-edit']").send_keys(Keys.ENTER)
         self.assertIn("Playstation 5 Pro 2tb Avenida Tecnologica", self.driver.find_element("xpath","//a[normalize-space()='Playstation 5 Pro 2tb Avenida Tecnologica']").text)
 
-    def test_title(self):
-
-        options = Options()
-        prefs = {
-            "profile.default_content_settings.popups": 0,
-            "directory_upgrade":True
-        }
-        options.add_experimental_option("prefs",prefs)
-        options.add_argument('start-maximized')
-        #options.add_argument("--headless")
-
-        #self.driver = webdriver.Chrome(service=ChromeService("../Drivers/chromedriver.exe"), options=options)
-
-        service = ChromeService(executable_path=ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=options)
-        
-        self.driver.get("https://www.google.com")
-        self.assertIn("Google", self.driver.title)
-
     def tearDown(self):
         self.driver.quit()
 
