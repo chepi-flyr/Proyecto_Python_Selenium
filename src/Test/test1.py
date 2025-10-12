8d860934ee16b25c449a227c598d00f311fbfe9b
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2025-10-12 14:07:34 -0600

    .

--- src/Test/test1.py
+++ src/Test/test1.py
@@ -10,7 +10,7 @@ class GoogleTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_title(self):
+    def test_mercado_libre(self):
 
         options = Options()
         prefs = {
@@ -31,8 +31,27 @@ class GoogleTest(unittest.TestCase):
         self.driver.find_element("xpath","//input[@id='cb1-edit']").send_keys(Keys.ENTER)
         self.assertIn("Playstation 5 Pro 2tb Avenida Tecnologica", self.driver.find_element("xpath","//a[normalize-space()='Playstation 5 Pro 2tb Avenida Tecnologica']").text)
 
+    def test_title(self):
+
+        options = Options()
+        prefs = {
+            "profile.default_content_settings.popups": 0,
+            "directory_upgrade":True
+        }
+        options.add_experimental_option("prefs",prefs)
+        options.add_argument('start-maximized')
+        #options.add_argument("--headless")
+
+        #self.driver = webdriver.Chrome(service=ChromeService("../Drivers/chromedriver.exe"), options=options)
+
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=options)
+        
+        self.driver.get("https://www.google.com")
+        self.assertIn("Google", self.driver.title)
+
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=r'..\src\reporthtmlrunner'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=r'Python con Selenium - Reporte Html Test Runner\src\reporthtmlrunner'))
